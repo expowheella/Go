e562768065ac8980710576c5efcbc0ffc9092d69
expowheella <bulat_man@mail.ru>
2022-10-27 13:48:19 +0300

    Closure

--- hello/functionClosure/functionClosure.go
+++ hello/functionClosure/functionClosure.go
@@ -5,7 +5,28 @@ import "fmt"
 func main() {
 	callFunction := myFunc()
 
-	fmt.Print(callFunction("Bulat"))
+	fmt.Printf("%v\n", callFunction("Bulat"))
+
+	a := 0
+	b := 1
+	c := 1
+
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+
+		fmt.Printf("%v %v %v ", f(a), f(b), f(c))
+		a = b + c
+		b = a + c
+		c = a + b
+	}
+
+}
+
+func fibonacci() func(x int) int {
+	result := func(x int) int {
+		return x
+	}
+	return result
 }
 
 func myFunc() func(s string) string {
